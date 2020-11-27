69fec72f32368b33084b1b39cc4b5e9e612eaf1e
Ayange Felix <ayangefelix8@gmail.com>
2020-11-26 22:42:17 -0800

    implement login logic

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,11 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 
+const signToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
 exports.signUp = async (req, res, next) => {
   try {
     const newUser = await User.create({
@@ -9,9 +14,8 @@ exports.signUp = async (req, res, next) => {
       password: req.body.password,
       passwordConfirm: req.body.passwordConfirm,
     });
-    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
-      expiresIn: process.env.JWT_EXPIRES_IN,
-    });
+    const token = signToken(newUser._id);
+
     if (!newUser) {
       return res.status(400).send({
         status: 'fail',
@@ -32,4 +36,36 @@ exports.signUp = async (req, res, next) => {
       message: error,
     });
   }
+};
+
+exports.login = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).send({
+      status: 'fail',
+      message: 'Please provide a username and password',
+    });
+  }
+
+  //check if user exist and password is actually correct
+  const user = await User.findOne({ email }).select('+password');
+
+  if (!user || !(await user.correctPassword(password, user.password))) {
+    return res.status(401).send({
+      status: 'fail',
+      message: 'Incorrect email or password',
+    });
+  }
+
+  const token = signToken(user._id);
+  return res.status(200).send({
+    status: 'success',
+    message: 'successfully logged in',
+    token,
+    data: {
+      name: user.name,
+      email: user.email,
+    },
+  });
 };
--- models/userModel.js
+++ models/userModel.js
@@ -19,6 +19,7 @@ const userSchema = Schema({
     type: String,
     required: [true, 'Please provide your password'],
     minlength: 5,
+    select: false,
   },
   passwordConfirm: {
     type: String,
@@ -45,6 +46,13 @@ userSchema.pre('save', async function (next) {
   this.passwordConfirm = undefined;
   next();
 });
+
+userSchema.methods.correctPassword = async function (
+  candidatePassword,
+  userPassword
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
 
 const User = model('User', userSchema);
 
