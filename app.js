08c0c1ce7538da3d1060e0001b19ab81353f2aea
Ayange Felix <ayangefelix8@gmail.com>
2021-02-15 15:28:49 +0100

    set up security http headers

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const dotenv = require('dotenv');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const tourRoutes = require('./routes/tourRoutes');
 const userRoutes = require('./routes/userRoutes');
@@ -10,12 +11,15 @@ const app = express();
 
 dotenv.config({ path: './config.env' });
 
-app.use(express.json());
+//set security http headers
+app.use(helmet());
 
+//Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+//limit request from same API
 const limiter = rateLimit({
   max: 50, //The max limit can be set anything
   windowMs: 60 * 60 * 1000,
@@ -24,8 +28,13 @@ const limiter = rateLimit({
 
 app.use('/api', limiter);
 
+//Body parser, reading data from body into req.body
+app.use(express.json());
+
+//Saving static files
 app.use(express.static(`${__dirname}/public`));
 
+//test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
