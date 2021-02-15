6ca503172887543a6e149034d89af86b91275fd1
Ayange Felix <ayangefelix8@gmail.com>
2021-02-15 14:46:51 +0100

    send jwt via cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -10,8 +10,22 @@ const signToken = (id) => {
   });
 };
 
+const cookieOptions = {
+  expires: new Date(
+    Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+  ),
+  httpOnly: true,
+};
+
+if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
+
+  res.cookie('jwt', token, cookieOptions);
+  //This remove password from the output
+  user.password = undefined;
+
   res.status(statusCode).send({
     status: 'success',
     message: 'successfully created',
